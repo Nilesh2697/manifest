11f1874058701805a035fbdb1f65c98c773ab27f
Nilesh2697 <99721281+Nilesh2697@users.noreply.github.com>
2023-05-20 22:55:39 +0530

    extrnsion

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let timeValus = {
+        hour:0,
+        min:0,
+        sec:0,
+    }
+    let isTiemrOn = false;
+    const display = document.getElementById("display");
+    const inputs = document.getElementById("inputs");
+
+    const hourInp = document.getElementById("hour-inp");
+    const minInp = document.getElementById("min-inp")
+    const secInp = document.getElementById("sec-inp")
+    
+    const submit = document.querySelector("#inputs > button");
+
+    submit.addEventListener("click",()=>{
+        
+    })
+})
